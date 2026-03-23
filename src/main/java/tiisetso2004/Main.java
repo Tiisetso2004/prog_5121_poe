4ca820ef077d3c7d6611ac037d1ccd5ad22727db
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-03-23 15:05:06 +0200

    Added metod call to main

--- src/main/java/tiisetso2004/Main.java
+++ src/main/java/tiisetso2004/Main.java
@@ -2,6 +2,7 @@ package tiisetso2004;
 
 public class Main {
     public static void main(String[] args) {
-        Login login = new Login(); //call the method
+        Login login = new Login();
+        login.registerUser(); //call the method
     }
 }
