600d89821f2f5e90801c0ead330ae45bea1d3911
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-03-23 14:55:38 +0200

    Refactored Login to handle validation

--- src/main/java/tiisetso2004/Login.java
+++ src/main/java/tiisetso2004/Login.java
@@ -1,59 +1,132 @@
 package tiisetso2004;
 
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.function.Predicate;
+import org.passay.*; // import passay library for password validation
+
+/*
+ * The login class will primarily consist entirely of utility functions.
+ */
 public class Login {
 
-    String fullname, username, cellphoneNumber, password;
+    private String fullName, username, cellphoneNumber, password;
+    private User user = new User(fullName, username, cellphoneNumber, password);
+    public static Scanner sc = new Scanner(System.in);
 
-    //call helper methods then return cutsom error messaging
-    public boolean checkFullName(String name) { 
-        fullname = name;
-        return Validator.validateFirstName(name);
-    }
+    static final Pattern CELLPHONE_REGEX = Pattern.compile("^\\+27[0-9]{9}$"); //basic regex for an SA cellphone number.
+    static final Pattern USERNAME_REGEX = Pattern.compile("^[a-zA-Z0-9_]{5,20}$"); //pattern for alphanumeric 5-20 character string including an underscore.
+    static final Pattern NAME_REGEX = Pattern.compile("^[\\p{L}_'\\- ]{2,50}$"); //basic regex to allow most names
 
-    public boolean checkUsername(String user) {
-        username = user;
-        return Validator.validateUsername(user);
-    }
+    static String usernameErrorMessage = "Incorrect username format.\nThe username must consist of 5-20 characters including an underscore";
+    static String cellphoneErrorMessage = "Error unrecognised cellphone number detected, please try again";
+    static String nameErrorMessage = "Ivalid entry detected, enter your proper name and surname";
+    static String passwordErrorMessage  = "";
 
-    public boolean checkPasswordComplexity(String pass) { //passay already returns error messaging
-        password = pass;
-        return Validator.validatePassword(password);
-    }
+    static String namePrompt = "Please enter your full name and surname only.";
+    static String userNamePrompt = "Please enter your new username";
+    static String passwordPrompt = "Please enter your new password";
+    static String cellphonePrompt = "Please enter your cellphone number in the format +27XXXXXXXXX";
 
-    public boolean checkCellphoneNumber(String cell) {
-        cellphoneNumber = cell;
-        return Validator.validateCellphoneNumber(cell);
-    }
-    
-    public String registerUser() { //returns login messaging, assignment instructions make messaging tedious.
-        String message = "";
-        
-        if (!checkFullName(fullname)) {
-            message = Validator.nameErrorMessage;
-            System.err.println(Validator.nameErrorMessage);
-        } 
-        else if (!checkUsername(username)) {
-            message = Validator.usernameErrorMessage;
-            System.err.println(Validator.usernameErrorMessage);
-        } 
-        else if (!checkCellphoneNumber(cellphoneNumber)) {
-            message = Validator.cellphoneErrorMessage;
-            System.err.println(Validator.cellphoneErrorMessage);
+    private static final PasswordValidator VALIDATOR = new PasswordValidator(Arrays.asList( 
+            new LengthRule(8,30), 
+            new CharacterRule(EnglishCharacterData.UpperCase,1), 
+            new CharacterRule(EnglishCharacterData.Special,1), 
+            new CharacterRule(EnglishCharacterData.Digit,1), 
+            new RepeatCharacterRegexRule(3), 
+            new WhitespaceRule() 
+    ));
+
+    public static boolean nullCheck(String input) { //checks if any string input is null or blank.
+        if (input == null || input.isBlank()) {  
+            System.err.println("Error: input cannot be empty or blank, please fill in this field");
+            return false;
         }
-        return message;     
+        return true;
     }
 
-    public boolean loginUser() { //TODO:find more suitable implement in Validator class.
-        return Validator.login();
+    public static boolean regexReader(Pattern regex, String input) { //helper function to compile regex patterns and return true or false
+        if (!nullCheck(input)) {
+            return false;
+        }
+        return regex.matcher(input).matches();
     }
 
-    public String returnLoginStatus(String fullName) {
-        String message;
-        if (!loginUser()) {
-            message = "Login was uncuccessful";   
+    public static boolean checkPasswordComplexity(String password) {
+        if (!nullCheck(password)) {
+            return false;
+        }
+        RuleResult result = VALIDATOR.validate(new PasswordData(password));
+        if (result.isValid()) {
+            System.out.println("valid password");
+            return true; //returns true if the argument is valid
         } else {
-            message = "Welcome back " + fullName + " it is great to see you again.";
+            List<String> messages = VALIDATOR.getMessages(result);
+            messages.forEach(System.out::println); //gets messages for invalid inputs from passay library
+            return false; //returns false if the argument is invalid
+        }           
+    }
+
+    public static boolean checkFirstName(String name) {
+        return regexReader(NAME_REGEX, name.trim());        
+    }  
+    
+    public static boolean checkUsername(String username) {
+        return regexReader(USERNAME_REGEX, username.trim());
+    }
+
+    public static boolean checkCellphoneNumber(String cell) {
+        return regexReader(CELLPHONE_REGEX, cell.trim());
+    }
+
+    public static String promptUntilValid(String prompt, Predicate <String> validator, String errorMessage) { //this is the validation input loop.
+        while (true) {
+            System.out.println(prompt);
+            String input = sc.nextLine();
+
+            if (validator.test(input)) {
+                return input;
+            } else {
+                System.err.println(errorMessage);
+            }
         }
-        return message;       
+    }
+
+    public String registerUser() { //returns messaging for false or true entries.
+        
+        fullName = promptUntilValid(namePrompt, Login::checkFirstName, nameErrorMessage);
+        username = promptUntilValid(userNamePrompt, Login::checkUsername, usernameErrorMessage);
+        cellphoneNumber = promptUntilValid(cellphonePrompt, Login::checkCellphoneNumber, cellphoneErrorMessage);
+        password = promptUntilValid(passwordPrompt, Login::checkPasswordComplexity, passwordErrorMessage);
+
+        user = new User(fullName, username, cellphoneNumber, password); //assing varuables to user object
+        UserDatabase.addUser(user);
+        returnLoginStatus(); //returns login status and has internal call to login.
+        return "User registered successfully: " + user.getFullName();
+    }
+
+    public boolean loginUser() { 
+        System.out.println("Re-enter your username to login");
+        String enterdUsername = sc.nextLine();
+        System.out.println("Re-enter your password to login");
+        String enteredPassword  = sc.nextLine();
+
+        return enterdUsername.equals(user.getUsername()) && enteredPassword.equals(user.getPassword());
+               
+        
+    }
+
+    public String returnLoginStatus() {
+        String message;
+        boolean validLogin = loginUser();
+
+        if (!validLogin) {
+            message = "Login Unsuccessful, please try again";
+            System.err.println(message);            
+        } else {
+            message = "Login Successful, Welcome back, It is great to see you again";
+            System.out.println(message);         
+        }
+        return message;   
     }
 }
