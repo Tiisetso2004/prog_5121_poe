8e925e13542eca487cfe42abf025530232c0c670
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-03-25 12:17:41 +0200

    Refactored 'User' to eliminate circular logic

--- src/main/java/tiisetso2004/User.java
+++ src/main/java/tiisetso2004/User.java
@@ -4,7 +4,7 @@ public class User {
 
     private String fullName, username, cellphoneNumber, password;
     
-    //call to parameterized constructor to auto assign first-time users
+    //call to parameterized constructor to auto-assign first time users
     public User (String name, String usr, String cell, String pass) { 
         this.fullName = name;
         this.username = usr;
@@ -14,25 +14,21 @@ public class User {
 
     //use setters and utilities to update fields
     public boolean updateFullName(String name) {
-        name =  Login.promptUntilValid(MessageLog.getNamePrompt(), Login::checkFullName, MessageLog.getNameErrorMessage(), MessageLog.getNameMessage());
         this.fullName = name;
         return true;
     }
 
     public boolean updateUsername(String usr) {
-        usr = Login.promptUntilValid(MessageLog.getUserNamePrompt(), Login::checkUsername, MessageLog.getUsernameErrorMessage(), MessageLog.getUsernameMessage());
         this.username = usr;
         return true;
     }
 
     public boolean updateCellphoneNumber(String cell) {
-        cell = Login.promptUntilValid(MessageLog.getCellphonePrompt(), Login::checkCellphoneNumber, MessageLog.getCellphoneErrorMessage(), MessageLog.getCellphoneMessage());
         this.cellphoneNumber = cell;
         return true;
     }
 
     public boolean updatePassword(String pass) {
-        pass = Login.promptUntilValid(MessageLog.getPasswordPrompt(), Login::checkPasswordComplexity, MessageLog.getPasswordErrorMessage(), MessageLog.getPasswordMessage());
         this.password = pass;
         return true;
     }
