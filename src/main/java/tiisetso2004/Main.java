43baea30e977d21aba8ea00dc890742c06ef2a4a
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-03-25 12:16:51 +0200

    Refactored 'Main' to reflect changes in 'Login'

--- src/main/java/tiisetso2004/Main.java
+++ src/main/java/tiisetso2004/Main.java
@@ -2,7 +2,7 @@ package tiisetso2004;
 
 public class Main {
     public static void main(String[] args) {
-        Login login = new Login();
-        login.registerUser(); //call the method
+        
+        Login loginApp = new Login();
     }
 }
