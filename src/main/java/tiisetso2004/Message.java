122d876bda8363f9849295f125ecab8e312ce267
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-04-21 15:24:16 +0200

    Created new classes on feature branch

---
+++ src/main/java/tiisetso2004/Message.java
@@ -0,0 +1,22 @@
+package tiisetso2004;
+
+public class Message {
+
+    public Message() {
+
+    }
+
+    public boolean checkMessageID() {}
+
+    public String checkRecepientCell() {}
+
+    public boolean createMessageHash() {}
+
+    public SentMessage() {} 
+
+    public printMessages() {}
+
+    public returnTotalMessages() {}
+
+    public void StoreMessage() {}
+}
---
+++ src/main/java/tiisetso2004/MessageData.java
@@ -0,0 +1,43 @@
+package tiisetso2004;
+
+public class MessageData {
+
+    private final String message, messageHash, sender, recepient; //ensure message contents or metadata cannot be changed
+    private final int messageID;
+
+    public MessageData(String message, String messageHash, String sender, String recipient, int messageID) {
+        this.message = message;
+        this.messageHash = messageHash;
+        this.messageID = messageID;
+        this.sender = sender;
+        this.recepient = recipient;
+    }
+    //getters only to retrieve values for metadata
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessageHash() {
+        return messageHash;
+    }
+
+    public int getMessageID() {
+        return messageID;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecepient() {
+        return recepient;
+    }
+
+    public void WriteMessage() {
+
+    }
+
+    
+    
+}
