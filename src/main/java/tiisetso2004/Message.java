18b7c226056b8dc8b7167a96506e49e1cc0ff946
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-04-23 16:04:12 +0200

    Implement GenerateMessageID method and edited cellphone number validation output

--- src/main/java/tiisetso2004/Message.java
+++ src/main/java/tiisetso2004/Message.java
@@ -5,6 +5,18 @@ public class Message {
     public Message() {
     }
 
+    public static String GenerateMessageID(int length) {
+        String charset = "4682519730"; //define characterset at random.
+        StringBuilder sb = new StringBuilder(length); //use string builder for a mutable string object.
+
+        for (int i = 0; i < length; i++) {
+            int index = (int) (charset.length() * Math.random()); //Math.Random returns 0.0 --> 1.0 cast it to int to get a valid whole number index.    
+            sb.append(charset.charAt(index)); //add a randomly selected character to the string builder object.
+        }
+        //return it as string matching method return type.
+        return sb.toString();
+    } 
+
     //checks if the ID exceeds the 10 character limit.
     public boolean checkMessageID(String ID) {
         if(Validator.nullCheck(ID) || ID.length() > 10) {
@@ -19,9 +31,10 @@ public class Message {
     //reuse static helper defined in Validator class for cellphone number validation.
     public String checkRecepientCell(String cellphoneNumber) {
         if(!Validator.checkCellphoneNumber(cellphoneNumber)) {
+            System.err.println(MessageLog.getCellphoneErrorMessage());
             return MessageLog.getCellphoneErrorMessage();
         } else {
-            System.err.println(MessageLog.getCellphoneErrorMessage());
+            System.out.println(MessageLog.getCellphoneErrorMessage());
             return cellphoneNumber;
         }
     }
