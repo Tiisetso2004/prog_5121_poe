2880c88905d75ca4414709a6941337b12061b3d2
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-06-01 13:08:25 +0200

    General refactor

--- src/main/java/tiisetso2004/MessageHandler.java
+++ src/main/java/tiisetso2004/MessageHandler.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class MessageHandler {
 
     public static boolean messageValidator(String m) {
-        boolean validMessage = Validator.NullCheck(m) && m.length()<=250;
+        boolean validMessage = Validator.nullCheck(m) && m.length()<=250;
         if(!validMessage) {
             System.out.println("Message is invalid:\nit exceeds 250 characters or is blank");
             return false;
@@ -16,11 +16,11 @@ public class MessageHandler {
     }
     //checks if the ID exceeds the 10-character limit.
     public static boolean validateMessageID(String ID) {
-        return ID.length() == 10 && Validator.NullCheck(ID);
+        return ID.length() == 10 && Validator.nullCheck(ID);
     }
 
     public static boolean messageOperationHandler(MessageData obj, List <MessageData> list) {
-        boolean isNullField = Validator.NullCheck(obj.getMessage()) && Validator.NullCheck(obj.getMessageHash()) && Validator.NullCheck(obj.getMessageID()) && Validator.NullCheck(obj.getRecipient());
+        boolean isNullField = Validator.nullCheck(obj.getMessage()) && Validator.nullCheck(obj.getMessageHash()) && Validator.nullCheck(obj.getMessageID()) && Validator.nullCheck(obj.getRecipient());
         if(list.isEmpty()||isNullField) {
             System.err.println("Error: attempted operation on empty list, or message object contains null fields");
             return false;
