67c4266f40e2ac87234766aed32238a1821dc8e3
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-06-12 20:30:57 +0200

    imports for JSON and File Handling

--- src/main/java/tiisetso2004/StorageManager.java
+++ src/main/java/tiisetso2004/StorageManager.java
@@ -1,6 +1,16 @@
 package tiisetso2004;
 
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 public class StorageManager {
 
