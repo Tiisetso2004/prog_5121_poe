fd7471ab7426a3e9402409036cbf6d55ae2942f5
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-06-12 20:22:04 +0200

    reflected changes

--- src/main/java/tiisetso2004/Validator.java
+++ src/main/java/tiisetso2004/Validator.java
@@ -41,7 +41,7 @@ public class Validator {
     /*helper function to receive regex patterns and return true or false.*/
     public static boolean regexReader(Pattern regex, String input) {
         //check if input is null
-        if (nullCheck(input)) {
+        if (!notNullCheck(input)) {
             return false;
         }
         //check if regex is null
